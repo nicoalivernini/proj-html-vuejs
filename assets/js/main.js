8829312c412002bef0a58ac7bcaba2cb96333c2f
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-21 15:39:56 +0200

    Inizio Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,39 @@
-// console.log('ok');
-//Instanziare Vue
+Vue.config.devtools = true;
+
+var app = new Vue ({
+
+  el: '#root',
+  data: {
+    menuNavbar: [
+      {
+        name: 'Home',
+        //link:
+      },
+      {
+        name: 'Courses',
+        //link:
+      },
+      {
+        name: 'Instructors',
+        //link: ,
+      },
+      {
+        name: 'Events',
+        //link:
+      },
+      {
+        name: 'Pages',
+        //link:
+      },
+      {
+        name: 'Elements',
+        //link:
+      }
+    ], //Chiusura menuNavbar
+  }, //Chiusura Data
+
+  methods: {
+
+  } //Chiusura Methods
+
+}); //Chiusura Vue
